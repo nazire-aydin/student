16c1cb9bc4db9c59fdc9bd018a8f651cebd1ee1b
Nazire Aydin <solmazaydin2009@hotmail.com>
2020-07-13 20:42:03 +0100

    Application class test for smoke test

--- src/test/java/com/nazire/Student/StudentApplicationTests.java
+++ src/test/java/com/nazire/Student/StudentApplicationTests.java
@@ -1,13 +1,26 @@
 package com.nazire.Student;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import uk.co.nazire.controller.StudentController;
+
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class StudentApplicationTests {
 
+	@Autowired
+	private StudentController controller;
+
 	@Test
 	void contextLoads() {
+        assertThat(controller).isNotNull();
 	}
 
 }
